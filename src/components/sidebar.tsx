a60135f834e501305fc53f5c061a65432e3d86ae
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-10 18:13:09 +0530

    added sidebar navlinks

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 
 const Sidebar = () => {
+    const location=useLocation()
+    console.log(location,"")
     return (
         <nav className='p-4 float-left flex-col flex bg-[#1c1c1c] lg:w-[240px] lg:h-screen sticky border-r-[#252525] border-r-[1px]'>
             <div className="flex profile gap-2 ml-2">
@@ -13,7 +15,7 @@ const Sidebar = () => {
             </div>
             <div className="nav-1 mt-4 flex-col">
                 <Link to="/">
-                    <div className="p-2 flex justify-between items-center hover:text-[#fff] transition-all duration-150 ease-out hover:ease-in text-[#858585] ">
+                    <div className={`navlinks  ${location.pathname=="/"?'active':'npg'}  `}>
                         <div className="flex gap-2 items-center">
                             <img className="w-[24px] h-[24px]" src="/src/assets/explore.svg" alt="explore icon" />
                             <p>Explore</p>
@@ -22,10 +24,28 @@ const Sidebar = () => {
                     </div>
                 </Link>
                 <Link to="/projects">
-                    <div className="p-2 flex justify-between items-center hover:text-[#fff] transition-all duration-150 ease-out hover:ease-in text-[#858585] ">
+                    <div className={`navlinks ${location.pathname=="/projects"?'active':'npg'}   `}>
                         <div className="flex gap-2 items-center">
                             <img className="w-[24px] h-[24px]" src="/src/assets/projects.svg" alt="projects icon" />
-                            <p>projects</p>
+                            <p>Projects</p>
+                        </div>
+                        <span className="px-1 rounded-sm border border-[#252525]">1</span>
+                    </div>
+                </Link>
+                <Link to="/services">
+                    <div className={`navlinks ${location.pathname=="/services"?'active':'npg'}   `}>
+                        <div className="flex gap-2 items-center">
+                            <img className="w-[24px] h-[24px]" src="/src/assets/services.svg" alt="services icon" />
+                            <p>Services</p>
+                        </div>
+                        <span className="px-1 rounded-sm border border-[#252525]">1</span>
+                    </div>
+                </Link>
+                <Link to="/about">
+                    <div className={`navlinks ${location.pathname=="/about"?'active':'npg'}   `}>
+                        <div className="flex gap-2 items-center">
+                            <img className="w-[24px] h-[24px]" src="/src/assets/about.svg" alt="about icon" />
+                            <p>About</p>
                         </div>
                         <span className="px-1 rounded-sm border border-[#252525]">1</span>
                     </div>
