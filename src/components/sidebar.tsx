72bf530518e0392daf786c591a055a0f95732823
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-11 23:48:23 +0530

    made adjustments

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,14 +6,14 @@ const Sidebar = () => {
     console.log(location,"")
     return (
         <nav className='p-4 float-left flex-col flex bg-[#1c1c1c] lg:w-[240px] lg:h-screen sticky border-r-[#252525] border-r-[1px]'>
-            <div className="flex profile gap-2 ml-2">
+            <div className="flex profile gap-2 ml-2 mt-2">
                 <img  className="rounded-full w-[40px] h-[40px]" src="/public/images/sam-profile.jpeg" alt="samjs picture" title='samjs picture' />
                 <div>
                     <h2 className='text-[#fff] font-semibold text-sm '>Samuvel Raja</h2>
                     <p className=' font-extralight text-sm text-[#858585]'>FrontEnd Developer</p>
                 </div>
             </div>
-            <div className="nav-1 mt-4 flex-col">
+            <div className="nav-1 mt-8 flex-col">
                 <Link to="/">
                     <div className={`navlinks  ${location.pathname=="/"?'active':'npg'}  `}>
                         <div className="flex gap-2 items-center">
@@ -61,7 +61,7 @@ const Sidebar = () => {
                 </Link>
             </div>
             <div className="nav-1 mt-4 flex-col">
-                <p  className="font-thin text-[12px] text-[#858585]">Goodreads</p>
+                <p  className="font-thin text-[12px] p-3 text-[#858585]">Goodreads</p>
                 <Link to="/blogs">
                     <div className={`navlinks  ${location.pathname=="/blogs"?'active':'npg'}  `}>
                         <div className="flex gap-2 items-center">
@@ -82,7 +82,7 @@ const Sidebar = () => {
                 </Link>
             </div>
             <div className="nav-1 mt-4 flex-col">
-                <p  className="font-thin text-[12px] text-[#858585]">connect</p>
+                <p className="font-thin text-[12px] p-3 text-[#858585]">connect</p>
                 <Link to="/contact">
                     <div className={`navlinks  ${location.pathname=="/contact"?'active':'npg'}  `}>
                         <div className="flex gap-2 items-center">
@@ -103,6 +103,17 @@ const Sidebar = () => {
                         </span>
                     </div>
                 </Link>
+                <Link to="https://www.linkedin.com/in/samuvel-raja-10321a220/">
+                    <div className={`navlinks npg `}>
+                        <div className="flex gap-2 items-center">
+                            <img className="w-[24px] h-[24px]" src="/src/assets/linkedin.svg" alt="linkedin icon" />
+                            <p>Linkedin</p>
+                        </div>
+                        <span className="px-1 rounded-sm border border-[#252525]">
+                            &#8599;
+                        </span>
+                    </div>
+                </Link>
                 <Link to="https://x.com/the_sam_js">
                     <div className={`navlinks npg `}>
                         <div className="flex gap-2 items-center">
@@ -114,17 +125,7 @@ const Sidebar = () => {
                         </span>
                     </div>
                 </Link>
-                <Link to="https://www.linkedin.com/in/samuvel-raja-10321a220/">
-                    <div className={`navlinks npg `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[20px] h-[20px]" src="/src/assets/linkedin.svg" alt="linkedin icon" />
-                            <p>Linkedin</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">
-                            &#8599;
-                        </span>
-                    </div>
-                </Link>
+                
             </div>
         </nav>
     )
