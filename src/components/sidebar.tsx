8bbdcbf3821ad3c52c29d83a7b426a072b0c43e2
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-14 23:50:11 +0530

    added experiences section

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
     const location=useLocation()
     console.log(location,"")
     return (
-        <nav className='p-4 float-left flex-col flex bg-[#1c1c1c] lg:w-[240px] lg:h-screen sticky border-r-[#252525] border-r-[1px]'>
+        <nav className='p-4 float-left flex-col flex bg-[#1c1c1c] lg:w-[240px] lg:h-screen fixed border-r-[#252525] border-r-[1px]'>
             <div className="flex profile gap-2 ml-2 mt-2">
                 <img  className="rounded-full w-[40px] h-[40px]" src="/public/images/sam-profile.jpeg" alt="samjs picture" title='samjs picture' />
                 <div>
--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -8,7 +8,7 @@ import nextImg from "../assets/nextjs.svg"
 import jsImg from "../assets/js.svg"
 import tailwindImg from "../assets/tailwind.svg"
 import threeImg from "../assets/three.svg"
-
+import project from "../assets/project.avif"
 
 
 const Explore = () => {
@@ -24,7 +24,7 @@ const Explore = () => {
         <h1 className="text-5xl pt-20 text-white font-semibold leading-[1.20]">
           Hey, I'm Sam.js, <br /> I code <Blinker texts={["FrontEnd", "Backend", "Middlewares", "Whatever..."]} />
           <br /> with <span className="text-[#858585]">javaScript</span></h1>
-        <p className="text-[#858585] text-[18px] my-6">
+        <p className="p1">
           I'm a Frontend Developer, I can build webapps with good looking user interfaces and awesome functionalities,
           i can also work on backend technologies like nodejs and expressjs
           worked on various products of different domains, take a look on my <Link className="plinks" to={"/projects"}>projects &#8599;</Link> and
@@ -40,8 +40,8 @@ const Explore = () => {
       </div>
       {/* stack section */}
       <div>
-        <div className="border border-[#252525] p-6 mt-10 rounded-[6px]">
-          <h2 className="mt-2 font-medium">Stack</h2>
+        <div className="card p-6 mt-10 ">
+          <h2 className="mt-2 text-lg font-medium">Stack</h2>
           <p className="text-[#858585] my-4">Tools and technologies that i work on</p>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
             <div className="flex p-[6px] border hover:border-[#393939] border-transparent rounded-sm hover:bg-[#2c2c2c]">
@@ -106,7 +106,49 @@ const Explore = () => {
       </div>
       {/* projects */}
       <div>
-        
+        <h2 className="mt-10 mb-4 text-lg font-medium">Projects</h2>
+        <div className="flex gap-8">
+          <div className="card  bg-[#2c2c2c]">
+            <div className="overflow-hidden">
+              <img src={project} alt="project" className=" hover:scale-110 transition-all" />
+            </div>
+            <div className="p-4">
+              <h3>Billify</h3>
+              <p className="text-[#858585] font-extralight">A digital invoice template for designers</p>
+            </div>
+          </div>
+          <div className="card  bg-[#2c2c2c]">
+            <div className="overflow-hidden">
+              <img src={project} alt="project" className=" hover:scale-110 transition-all" />
+            </div>
+            <div className="p-4">
+              <h3>Billify</h3>
+              <p className="text-[#858585] font-extralight">A digital invoice template for designers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* experience */}
+      <div>
+        <h2 className="mt-10  text-lg font-medium">Experiences</h2>
+        <div className="card mt-6 p-6 bg-[#1c1c1c]">
+          <h3 className="text-[16px] mb-4 font-medium">Frontend Developer Lead - Zemuria.inc <span className="plinks">2023-2024</span></h3>
+          <ul className="p1 p-4">
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et distinctio vitae tempore consectetur aut tempora. Perspiciatis fugiat deserunt fugit soluta, nobis iste, fuga sed eos ab numquam laudantium. Non, inventore.</li>
+            <li>Asperiores modi, alias inventore cum molestiae doloribus accusamus ad recusandae autem fuga. Dignissimos magni, perspiciatis, soluta excepturi et enim obcaecati tempore corrupti dolor eius dolorem, maiores aliquam reprehenderit itaque placeat?</li>
+            <li>Eum voluptate laboriosam quasi qui ad quas voluptas, praesentium voluptatem, reiciendis sed a amet, sit iusto in minus magnam fugit quis libero commodi ipsum sequi temporibus. Veniam rem alias quaerat?</li>
+            <li>Quis cumque ratione enim dolores maxime quibusdam vitae quas corporis maiores aut eos dignissimos nihil quae aliquam rem blanditiis alias quaerat, natus, libero, itaque temporibus? Nisi consequatur iure ea et?</li>
+          </ul>
+        </div>
+        <div className="card mt-6 p-6 bg-[#1c1c1c]">
+          <h3 className="text-[16px] mb-4 font-medium">Frontend Developer - Zemuria.inc <span className="plinks">2022-2023</span></h3>
+          <ul className="p1">
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et distinctio vitae tempore consectetur aut tempora. Perspiciatis fugiat deserunt fugit soluta, nobis iste, fuga sed eos ab numquam laudantium. Non, inventore.</li>
+            <li>Asperiores modi, alias inventore cum molestiae doloribus accusamus ad recusandae autem fuga. Dignissimos magni, perspiciatis, soluta excepturi et enim obcaecati tempore corrupti dolor eius dolorem, maiores aliquam reprehenderit itaque placeat?</li>
+            <li>Eum voluptate laboriosam quasi qui ad quas voluptas, praesentium voluptatem, reiciendis sed a amet, sit iusto in minus magnam fugit quis libero commodi ipsum sequi temporibus. Veniam rem alias quaerat?</li>
+            <li>Quis cumque ratione enim dolores maxime quibusdam vitae quas corporis maiores aut eos dignissimos nihil quae aliquam rem blanditiis alias quaerat, natus, libero, itaque temporibus? Nisi consequatur iure ea et?</li>
+          </ul>
+        </div>
       </div>
     </>
    
