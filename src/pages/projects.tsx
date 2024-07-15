c1413d73216507821d95c82e0c230acf283f185d
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-15 23:59:30 +0530

    added project page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,32 @@
 
+import project from "../assets/project.avif"
+
 const Projects = () => {
   return (
     <div>
-        <h2 className="text-3xl">Projects</h2>
+        <div>
+        <h2 className="mt-10 mb-4 text-lg font-medium">Projects</h2>
+        <div className="flex gap-8">
+          <div className="card  bg-[#2c2c2c]">
+            <div className="overflow-hidden">
+              <img src={project} alt="project" className=" hover:scale-110 transition-all" />
+            </div>
+            <div className="p-4">
+              <h3>Billify</h3>
+              <p className="text-[#858585] font-extralight">A digital invoice template for designers</p>
+            </div>
+          </div>
+          <div className="card  bg-[#2c2c2c]">
+            <div className="overflow-hidden">
+              <img src={project} alt="project" className=" hover:scale-110 transition-all" />
+            </div>
+            <div className="p-4">
+              <h3>Billify</h3>
+              <p className="text-[#858585] font-extralight">A digital invoice template for designers</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
