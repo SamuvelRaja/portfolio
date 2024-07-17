182d4c53110141c701ea45f229b381f261f723ee
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-17 15:22:26 +0530

    added ctrl navigation

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import exploreIcon from "/src/assets/explore.svg";
 import projectsIcon from "/src/assets/projects.svg";
 import stackIcon from "/src/assets/stacks.svg";
@@ -10,135 +10,252 @@ import contactIcon from "/src/assets/contact.svg";
 import githubIcon from "/src/assets/git.svg";
 import linkedinIcon from "/src/assets/linkedin.svg";
 import xIcon from "/src/assets/x.svg";
+import { useEffect } from "react";
 
 const Sidebar = () => {
-    const location=useLocation()
-    console.log(location,"")
-    return (
-        <nav className='p-4 float-left flex-col flex bg-[#1c1c1c] lg:w-[240px] lg:h-screen fixed top-0 border-r-[#252525] border-r-[1px]'>
-            <div className="flex profile gap-2 ml-2 mt-2">
-                <img  className="rounded-full w-[40px] h-[40px]" src="/public/images/sam-profile.jpeg" alt="samjs picture" title='samjs picture' />
-                <div>
-                    <h2 className='text-[#fff] font-semibold text-sm '>Samuvel Raja</h2>
-                    <p className=' font-extralight text-sm text-[#858585]'>FrontEnd Developer</p>
-                </div>
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    function pushto(e: KeyboardEvent) {
+      if (e.ctrlKey) {
+        switch (e.key) {
+          case "1":
+            navigate("/");
+            break;
+          case "2":
+            navigate("/projects");
+            break;
+          case "3":
+            navigate("/stack");
+            break;
+          case "4":
+            navigate("/services");
+            break;
+          case "5":
+            navigate("/aboutme");
+            break;
+          case "6":
+            navigate("/resources");
+            break;
+          case "7":
+            navigate("/blogs");
+            break;
+          case "8":
+            navigate("/contact");
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    document.addEventListener("keydown", (e: KeyboardEvent) => pushto(e));
+    return () => {
+      document.removeEventListener("keydown", (e: KeyboardEvent) => pushto(e));
+    };
+  }, []);
+  return (
+    <nav className="p-4 float-left flex-col flex bg-[#1c1c1c] lg:w-[240px] lg:h-screen fixed top-0 border-r-[#252525] border-r-[1px]">
+      <div className="flex profile gap-2 ml-2 mt-2">
+        <img
+          className="rounded-full w-[40px] h-[40px]"
+          src="/public/images/sam-profile.jpeg"
+          alt="samjs picture"
+          title="samjs picture"
+        />
+        <div>
+          <h2 className="text-[#fff] font-semibold text-sm ">Samuvel Raja</h2>
+          <p className=" font-extralight text-sm text-[#858585]">
+            FrontEnd Developer
+          </p>
+        </div>
+      </div>
+      <div className="nav-1 mt-8 flex-col">
+        <Link to="/">
+          <div
+            className={`navlinks  ${
+              location.pathname == "/" ? "active" : "npg"
+            }  `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={exploreIcon}
+                alt="explore icon"
+              />
+              <p>Explore</p>
             </div>
-            <div className="nav-1 mt-8 flex-col">
-                <Link to="/">
-                    <div className={`navlinks  ${location.pathname=="/"?'active':'npg'}  `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={exploreIcon} alt="explore icon" />
-                            <p>Explore</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">1</span>
-                    </div>
-                </Link>
-                <Link to="/projects">
-                    <div className={`navlinks ${location.pathname=="/projects"?'active':'npg'}   `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={projectsIcon} alt="projects icon" />
-                            <p>Projects</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">2</span>
-                    </div>
-                </Link>
-                <Link to="/stack">
-                    <div className={`navlinks ${location.pathname=="/stack"?'active':'npg'}   `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={stackIcon} alt="stack icon" />
-                            <p>Stack</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">3</span>
-                    </div>
-                </Link>
-                <Link to="/services">
-                    <div className={`navlinks ${location.pathname=="/services"?'active':'npg'}   `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={servicesIcon} alt="services icon" />
-                            <p>Services</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">4</span>
-                    </div>
-                </Link>
-                <Link to="/aboutme">
-                    <div className={`navlinks ${location.pathname=="/aboutme"?'active':'npg'}   `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={aboutIcon} alt="about icon" />
-                            <p>About me</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">5</span>
-                    </div>
-                </Link>
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+1 to navigate">1</span>
+          </div>
+        </Link>
+        <Link to="/projects">
+          <div
+            className={`navlinks ${
+              location.pathname == "/projects" ? "active" : "npg"
+            }   `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={projectsIcon}
+                alt="projects icon"
+              />
+              <p>Projects</p>
             </div>
-            <div className="nav-1 mt-4 flex-col">
-                <p  className="font-light text-[12px] p-3 text-[#858585]">Goodreads</p>
-                <Link to="/blogs">
-                    <div className={`navlinks  ${location.pathname=="/blogs"?'active':'npg'}  `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={feedIcon} alt="blog icon" />
-                            <p>Blogs</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">6</span>
-                    </div>
-                </Link>
-                <Link to="/resources">
-                    <div className={`navlinks ${location.pathname=="/resources"?'active':'npg'}   `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={thoughtIcon} alt="resources icon" />
-                            <p>Resources</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">7</span>
-                    </div>
-                </Link>
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+2 to navigate">2</span>
+          </div>
+        </Link>
+        <Link to="/stack">
+          <div
+            className={`navlinks ${
+              location.pathname == "/stack" ? "active" : "npg"
+            }   `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={stackIcon}
+                alt="stack icon"
+              />
+              <p>Stack</p>
             </div>
-            <div className="nav-1 mt-4 flex-col">
-                <p className="font-light text-[12px] p-3 text-[#858585]">connect</p>
-                <Link to="/contact">
-                    <div className={`navlinks  ${location.pathname=="/contact"?'active':'npg'}  `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={contactIcon} alt="contact icon" />
-                            <p>Contact</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">8</span>
-                    </div>
-                </Link>
-                <Link to="https://github.com/SamuvelRaja">
-                    <div className={`navlinks npg `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={githubIcon} alt="github icon" />
-                            <p>Github</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">
-                            &#8599;
-                        </span>
-                    </div>
-                </Link>
-                <Link to="https://www.linkedin.com/in/samuvel-raja-10321a220/">
-                    <div className={`navlinks npg `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[24px] h-[24px]" src={linkedinIcon} alt="linkedin icon" />
-                            <p>Linkedin</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">
-                            &#8599;
-                        </span>
-                    </div>
-                </Link>
-                <Link to="https://x.com/the_sam_js">
-                    <div className={`navlinks npg `}>
-                        <div className="flex gap-2 items-center">
-                            <img className="w-[20px] h-[20px]" src={xIcon} alt="x icon" />
-                            <p>Twitter</p>
-                        </div>
-                        <span className="px-1 rounded-sm border border-[#252525]">
-                            &#8599;
-                        </span>
-                    </div>
-                </Link>
-                
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+3 to navigate">3</span>
+          </div>
+        </Link>
+        <Link to="/services">
+          <div
+            className={`navlinks ${
+              location.pathname == "/services" ? "active" : "npg"
+            }   `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={servicesIcon}
+                alt="services icon"
+              />
+              <p>Services</p>
             </div>
-        </nav>
-    )
-}
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+4 to navigate">4</span>
+          </div>
+        </Link>
+        <Link to="/aboutme">
+          <div
+            className={`navlinks ${
+              location.pathname == "/aboutme" ? "active" : "npg"
+            }   `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={aboutIcon}
+                alt="about icon"
+              />
+              <p>About me</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+5 to navigate">5</span>
+          </div>
+        </Link>
+      </div>
+      <div className="nav-1 mt-4 flex-col">
+        <p className="font-light text-[12px] p-3 text-[#858585]">Goodreads</p>
+        <Link to="/resources">
+          <div
+            className={`navlinks ${
+              location.pathname == "/resources" ? "active" : "npg"
+            }   `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={thoughtIcon}
+                alt="resources icon"
+              />
+              <p>Resources</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+6 to navigate">6</span>
+          </div>
+        </Link>
+        <Link to="/blogs">
+          <div
+            className={`navlinks  ${
+              location.pathname == "/blogs" ? "active" : "npg"
+            }  `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={feedIcon}
+                alt="blog icon"
+              />
+              <p>Blogs</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+7 to navigate">7</span>
+          </div>
+        </Link>
+      </div>
+      <div className="nav-1 mt-4 flex-col">
+        <p className="font-light text-[12px] p-3 text-[#858585]">connect</p>
+        <Link to="/contact">
+          <div
+            className={`navlinks  ${
+              location.pathname == "/contact" ? "active" : "npg"
+            }  `}
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={contactIcon}
+                alt="contact icon"
+              />
+              <p>Contact</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]" title="ctrl+8 to navigate">8</span>
+          </div>
+        </Link>
+        <Link to="https://github.com/SamuvelRaja">
+          <div className={`navlinks npg `}>
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={githubIcon}
+                alt="github icon"
+              />
+              <p>Github</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]">
+              &#8599;
+            </span>
+          </div>
+        </Link>
+        <Link to="https://www.linkedin.com/in/samuvel-raja-10321a220/">
+          <div className={`navlinks npg `}>
+            <div className="flex gap-2 items-center">
+              <img
+                className="w-[24px] h-[24px]"
+                src={linkedinIcon}
+                alt="linkedin icon"
+              />
+              <p>Linkedin</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]">
+              &#8599;
+            </span>
+          </div>
+        </Link>
+        <Link to="https://x.com/the_sam_js">
+          <div className={`navlinks npg `}>
+            <div className="flex gap-2 items-center">
+              <img className="w-[20px] h-[20px]" src={xIcon} alt="x icon" />
+              <p>Twitter</p>
+            </div>
+            <span className="px-1 rounded-sm border border-[#252525]">
+              &#8599;
+            </span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
