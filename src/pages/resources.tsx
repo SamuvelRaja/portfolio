aeaea9243aa0d10a9342c97836b69e8597c65139
SamuvelRaja <josephsamuvel001@gmail.com>
2024-07-17 21:23:25 +0530

    fix: removed deleted imports and fixed iframe errors

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -3,29 +3,36 @@ const Resources = () => {
   return (
     <div className="pb-16">
       <h1 className="title">Resources</h1>
-      <p className="p2 mt-2 mb-12">Im a firm of knowledge sharing here Im sharing the resources that made better</p>
+      <p className="p2 mt-2 mb-12">Im a firm believer of knowledge sharing, here Im sharing the resources that made me better</p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8 pb-8">
-          <div className="card  bg-[#2c2c2c]">
+          <div className="card bg-[#2c2c2c]">
             <div className="overflow-hidden">
-              <iframe width="414" height="232" src="https://www.youtube.com/embed/pN6jk0uUrD8?si=qNmt0X0SW7osXDxK" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullscreen></iframe>
+              <iframe
+                width="414"
+                height="232"
+                src="https://www.youtube.com/embed/pN6jk0uUrD8?si=qNmt0X0SW7osXDxK"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
             </div>
             <div className="p-4">
-              <h3>Namaste javaScript</h3>
-              <p className="text-[#858585] font-extralight">deep dive into the js with Akshay Saini</p>
+              <h3>Namaste JavaScript</h3>
+              <p className="text-[#858585] font-extralight">Deep dive into JavaScript with Akshay Saini</p>
             </div>
           </div>
-          <div className="card  bg-[#2c2c2c]">
+          <div className="card bg-[#2c2c2c]">
             <div className="overflow-hidden">
-              <iframe width="414" height="232" src="https://www.youtube.com/embed/cCOL7MC4Pl0?si=ea-zHPm-pZEsVBgz" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullscreen></iframe>
-            </div>
-            <div className="p-4">
-              <h3>Understanding Event Loop</h3>
-              <p className="text-[#858585] font-extralight">Event Loop masterclass by Jake Archibald</p>
-            </div>
-          </div>
-          <div className="card  bg-[#2c2c2c]">
-            <div className="overflow-hidden">
-              <iframe width="414" height="232" src="https://www.youtube.com/embed/cCOL7MC4Pl0?si=ea-zHPm-pZEsVBgz" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullscreen></iframe>
+              <iframe
+                width="414"
+                height="232"
+                src="https://www.youtube.com/embed/cCOL7MC4Pl0?si=ea-zHPm-pZEsVBgz"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
             </div>
             <div className="p-4">
               <h3>Understanding Event Loop</h3>
@@ -33,7 +40,7 @@ const Resources = () => {
             </div>
           </div>
         </div>
-    </div>
+      </div>
   )
 }
 
